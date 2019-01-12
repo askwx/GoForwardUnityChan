c366390f87a91941e77b70d45f9146236736623c
AsukaKawaguchi <jet-rag@live.jp>
2019-01-12 15:35:46 +0200

    Lesson7の課題です。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    //キューブの移動速度
+    private float speed = -0.2f;
+
+    //消滅位置
+    private float deadLine = -10;
+
+
+    //ブロック音
+    private AudioSource sound01;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        
+        //AudioSourceコンポーネントを取得
+        sound01 = GetComponent<AudioSource>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        //画面外に出たら破棄する
+        if(transform.position.x < this.deadLine){
+            Destroy(gameObject);
+        }
+		
+	}
+
+    //音
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if((collision.gameObject.tag == "block" )&& (collision.gameObject.tag =="ground")){
+            sound01.PlayOneShot(sound01.clip);
+
+        }
+        
+       
+
+
+        }
+    }
+
